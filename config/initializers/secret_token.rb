6bea62479716072945d013ef01a4b24d1a3587d1
Driftkikker Productions <info@driftkikkerproductions.com>
2013-02-14 20:42:03 +0100

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'eb6f1b540f713918b98c39e8b9eada9682fc8556fc59a0168a9e8866d64e64d95f5d07ae5486347640ba8a383a205b8ec7f9450f82a1ccd51b1e5d90015008ac'
